9e11d208daec9659c2bafd99b88ebadff823575b
kuramoto-k <koutaroukuramoto@gmail.com>
2020-07-07 07:03:21 +0900

    fix yml

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,6 +3,7 @@
   <agile class="main" ref="main" :options="options1" :as-nav-for="asNavFor1">
     <div class="slide" v-for="(slide, index) in slides" :key="index" :class="`slide--${index}`"><img :src="slide"/></div>
   </agile>
+  
   <agile class="thumbnails" ref="thumbnails" :options="options2" :as-nav-for="asNavFor2">
     <div class="slide slide--thumbniail" v-for="(slide, index) in slides" :key="index" :class="`slide--${index}`" @click="$refs.thumbnails.goTo(index)"><img :src="slide"/></div>
     <template slot="prevButton"><i class="fas fa-chevron-left"></i></template>
